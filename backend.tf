887108bd844315c956878a7bb23d47525ed24e09
Pratik <khotpratik625@gmail.com>
2026-04-02 18:04:41 +0530

    updated backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "tfstatebucket-690509489991-us-east-1-an"
+    bucket = "tfstatebucket-916393717813-us-east-1-an"
     key    = "docker-hon/dev/terraform.tfstate"
     region = "us-east-1"
     use_lockfile = true
