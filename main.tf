cae47fd4e65f20ba6039cc19a3cbdd005e39201a
Pratik <khotpratik625@gmail.com>
2026-04-03 00:05:15 +0530

    updated main tf

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
 resource "aws_vpc" "hon-practise-vpc" {
     cidr_block = "10.0.0.0/16"
     region = var.region
+    enable_dns_hostnames = true
+    enable_dns_support = true
     tags = {
         Name = "hon-practise-vpc"
     }
